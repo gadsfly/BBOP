c5055848764e2c3b6ddc399785eff21e08c5ec0a
gadsfly <amabrodn@gmail.com>
2024-08-06 20:39:16 +0000

    cut the end of the array for both sampleID and dataframe

--- utlis/convert_calib_utlis/convert_sync_and_label.py
+++ utlis/convert_calib_utlis/convert_sync_and_label.py
@@ -18,28 +18,47 @@ def count_leading_nans(arr):
 def convert_sync(old_calib_path, new_calib_name):
     old_calib_data = sio.loadmat(old_calib_path)
     sync = old_calib_data['sync']
+    ew_sync = copy.deepcopy(sync)
     new_sync = copy.deepcopy(sync)
 
     dff = 'data_frame'
     sid = 'data_sampleID'
+
+    min_length = len(sync[0][0][sid][0][0][0])
 
     for camera in range(6):
         old_data_sampleID = sync[camera][0][sid][0][0][0]
 
         # # change all sampleID to actually one, note that this starts with 1, instead of 0. the old one started with 0
         lenn = len(old_data_sampleID)
-        new_data_sampleID = list(range(1,lenn+1))
-        new_sync[camera][0][sid][0][0][0] = new_data_sampleID
+        
+        new_data_sampleID = np.arange(1, lenn + 1, dtype=np.float64) # list(range(1,lenn+1))
+        ew_sync[camera][0][sid][0][0] = new_data_sampleID
 
         # now, if sampleID starts with nan, then we need to adjust the new data_frame for it. if it is not nan, then it stays the same
-        if old_data_sampleID[0]==0:
+        if np.isnan(old_data_sampleID[0]): #old_data_sampleID[0]==0:
             # print(f'camera {camera+1} does not have nan, remains unchanged')
-            continue
-        else:
             nan_counts = count_leading_nans(old_data_sampleID)
             # print(f'camera {camera+1} has nan count of {nan_counts}, now starts with {nan_counts+1}')
-            new_data_frame = list(range(nan_counts+1,lenn+1))
-            new_sync[camera][0][dff][0][0] = np.array(new_data_frame, dtype=np.float64)
+            new_data_frame = np.arange(nan_counts + 1, lenn + 1, dtype=np.float64) # list(range(nan_counts+1,lenn+1))
+            ew_sync[camera][0][dff][0][0] = new_data_frame # np.array(new_data_frame, dtype=np.float64)
+            min_length = min(min_length, len(new_data_frame))
+        else:
+            ew_sync[camera][0][dff][0][0] = sync[camera][0][dff][0][0][0]
+            min_length = min(min_length, lenn)
+        
+        # print(min_length)
+    
+    # print("final", min_length)
+
+
+    # Cut the end of other data frames and sample IDs to match the minimum length
+    for camera in range(6):
+        # print("0", len(ew_sync[camera][0][dff][0][0][:min_length]))
+        new_sync[camera][0][dff][0][0] = ew_sync[camera][0][dff][0][0][:min_length]
+        # print("1", len(ew_sync[camera][0][dff][0][0][:min_length]))
+        new_sync[camera][0][sid][0][0] = ew_sync[camera][0][sid][0][0][:min_length]
+        # print("2", len(ew_sync[camera][0][sid][0][0][:min_length]))
     
     base_path = os.path.dirname(old_calib_path)
     save_path = os.path.join(base_path, new_calib_name)
