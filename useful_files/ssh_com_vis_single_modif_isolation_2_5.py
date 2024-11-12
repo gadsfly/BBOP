72b9f5d612ca6736b30f7a38f233aaf6065aac81
gadsfly <amabrodn@gmail.com>
2024-11-12 12:00:57 -0500

    added some prior evaluations, tested with the from raw exe, adapted sophie's speed historgram as well

---
+++ useful_files/ssh_com_vis_single_modif_isolation_2_5.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 23 14:01:45 2022
+
+@author: 13190044511
+"""
+import os
+import numpy as np
+import scipy.io as sio
+import imageio
+import tqdm
+from projection import *
+import connectivity
+
+import matplotlib
+#matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.animation import FFMpegWriter
+
+def find_calib_file(base_folder):
+    for file_name in os.listdir(base_folder):
+        if file_name.endswith('label3d_dannce.mat'):
+            return os.path.join(base_folder, file_name)
+    return None
+
+###############################################################################################################
+base_path =  '/hpc/group/tdunn/Bryan_Rigs/BigOpenField/24summ/2024_08_08/20240628_PMC_r1_11_26'
+cam = 'Camera2' 
+pred_folder = 'COM/predict00'
+com_file = os.path.join(base_path,pred_folder,'com3d0.mat')
+N_FRAMES = 2
+START_FRAME = 0
+##########################################################################
+
+
+video_path = os.path.join(base_path, 'videos',cam,'0.mp4')
+label3d_path = find_calib_file(base_path)
+# label3d_path = '/hpc/group/tdunn/Bryan_Rigs/BigOpenField/24summ/2024_06_28/1686941_left_right_2/pos_synced_1686941_left_right_2_2024_06_28_1686941_left_label3d_dannce.mat' #calib
+# pred_path = os.path.join(base_path, pred_folder, 'save_data_AVG0.mat') #  smoothed_prediction_AVG0.mat save_data_AVG0.mat
+
+# ANIMAL= 'mouse20' #'mouse22'
+vid_title = f'z_combined_{cam}_{N_FRAMES}_from_{START_FRAME}'
+VID_NAME = vid_title + '.mp4'
+# COLOR = connectivity.COLOR_DICT[ANIMAL]
+# CONNECTIVITY = connectivity.CONNECTIVITY_DICT[ANIMAL]
+save_path = os.path.join(base_path, pred_folder, 'vis') #os.path.join(pred_path, 'vis')
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+
+com_data = sio.loadmat(com_file)
+###############################################################################################################
+# # load camera parameterss
+cameras = load_cameras(label3d_path)
+
+# # get dannce predictions
+# pred_3d = sio.loadmat(pred_path)['pred'][START_FRAME: START_FRAME+N_FRAMES]
+
+# # compute projections
+# pred_2d = {}
+# pose_3d = np.transpose(pred_3d, (0, 2, 1))
+# pts = np.reshape(pose_3d, (-1, 3))
+
+
+# # get the 2d projection
+# projpts = project_to_2d(pts,
+#                         cameras[cam]["K"],
+#                         cameras[cam]["r"],
+#                         cameras[cam]["t"])[:, :2]
+
+# projpts = distortPoints(projpts,
+#                         cameras[cam]["K"],
+#                         np.squeeze(cameras[cam]["RDistort"]),
+#                         np.squeeze(cameras[cam]["TDistort"]))
+# projpts = projpts.T
+# projpts = np.reshape(projpts, (N_FRAMES, -1, 2))
+# pred_2d[cam] = projpts
+
+
+# del projpts, pred_3d
+
+
+###############################3
+# for com
+pts_com = com_data['com'][START_FRAME: START_FRAME+N_FRAMES]
+pred_2d_com = {}
+# Get the 2d projection for com
+projpts_com = project_to_2d(pts_com,
+                            cameras[cam]["K"],
+                            cameras[cam]["r"],
+                            cameras[cam]["t"])[:, :2]
+
+projpts_com = distortPoints(projpts_com,
+                            cameras[cam]["K"],
+                            np.squeeze(cameras[cam]["RDistort"]),
+                            np.squeeze(cameras[cam]["TDistort"]))
+projpts_com = projpts_com.T
+projpts_com = np.reshape(projpts_com, (N_FRAMES, -1, 2))
+pred_2d_com[cam] = projpts_com
+del projpts_com
+#####################3
+
+
+# open videos
+vids = imageio.get_reader(video_path)
+
+# set up video writer
+metadata = dict(title='combined_visualization', artist='Matplotlib')
+writer = FFMpegWriter(fps=20, metadata=metadata) # orig fps = 30.
+
+###############################################################################################################
+fig = plt.figure()
+plt.rcParams['figure.figsize'] = (6, 6)
+
+
+def adjust_viewport(kpts_2d, margin=70):
+    """
+    Adjust the plot's viewport based on keypoints.
+    :param kpts_2d: Keypoints for the current frame.
+    :param margin: Extra space around the keypoints to ensure they are not on the edge.
+    """
+    # This method is way too shaky
+    # min_x, max_x = np.min(kpts_2d[:, 0]), np.max(kpts_2d[:, 0])
+    # min_y, max_y = np.min(kpts_2d[:, 1]), np.max(kpts_2d[:, 1])
+    # plt.xlim([min_x - margin, max_x + margin])
+    # plt.ylim([max_y + margin, min_y - margin])
+
+    center_x = np.mean(kpts_2d[:, 0])
+    center_y = np.mean(kpts_2d[:, 1])
+    plt.xlim([center_x - margin, center_x + margin])
+    plt.ylim([center_y + margin, center_y - margin])
+
+
+with writer.saving(fig, os.path.join(save_path, "vis_"+VID_NAME), dpi=300):
+    for curr_frame in tqdm.tqdm(range(N_FRAMES)):
+        plt.clf()
+        # grab images
+        imgs = [vids.get_data(curr_frame+START_FRAME)][0]
+        # kpts_2d = pred_2d[cam][curr_frame]
+        
+        # temp_kpts_2d = np.r_[kpts_2d[0:6,:],kpts_2d[8:,:]]
+
+        # Plot com keypoints
+        kpts_2d_com = pred_2d_com[cam][curr_frame]
+        temp_kpts_2d_com = np.r_[kpts_2d_com[0:6,:],kpts_2d_com[8:,:]]
+        
+        # Zoom in based on keypoints
+        # adjust_viewport(temp_kpts_2d_com, margin=450)  # Adjust margin as needed for best fit 150 is good.
+
+
+        plt.imshow(imgs)
+        
+        # Plot com points
+        plt.scatter(kpts_2d_com[:, 0], kpts_2d_com[:, 1], marker='.', color='red', linewidths=2, alpha=0.5)
+
+        # plt.scatter(temp_kpts_2d[:, 0], temp_kpts_2d[:, 1], marker='.', color='white', linewidths=2, alpha=0.5) #point size
+
+        # for color, (index_from, index_to) in zip(COLOR, CONNECTIVITY):
+        #     xs, ys = [np.array([kpts_2d[index_from, j], kpts_2d[index_to, j]]) for j in range(2)]
+        #     plt.plot(xs, ys, c=color, lw=2) #line error
+        #     del xs, ys
+
+        plt.title(vid_title)
+        plt.axis("off")
+        
+        writer.grab_frame()
