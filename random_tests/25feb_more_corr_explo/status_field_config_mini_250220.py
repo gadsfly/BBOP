cfda788c4019031bafd62409558b04d697e851a9
gadsfly <amabrodn@gmail.com>
2025-02-24 17:32:35 -0500

    backup_everything_so_messy....okay

---
+++ random_tests/25feb_more_corr_explo/status_field_config_mini_250220.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.append(os.path.abspath('..'))
+# from utlis.scan_engine_utlis.scan_engine_utlis import is_special_date
+
+STATUS_FIELDS_CONFIG = {
+
+    'minian': {
+        'default': 0,
+        'conditions': [
+            {
+                'condition': lambda **kwargs: os.path.exists(
+                    os.path.join(kwargs['subfolder_path'], 'minian_dataset.nc')
+                ),
+                'value': 1  # Mark 1 if "minian_dataset.nc" is found
+            }
+        ]
+    },
+
+    'valid_rec': {
+        # If we want “0” for missing 4.avi and “1” if found:
+        'default': 0,  
+        'conditions': [
+            {
+                'condition': lambda **kwargs: os.path.exists(
+                    os.path.join(kwargs['subfolder_path'], '4.avi')
+                ),
+                'value': 1
+            }
+        ]}
+}
