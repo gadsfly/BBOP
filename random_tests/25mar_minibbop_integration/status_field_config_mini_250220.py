5a7f5ceb68b1b6e03a665434897327366563b8da
gadsfly <amabrodn@gmail.com>
2025-03-19 13:45:38 -0400

    more updates

--- random_tests/25mar_minibbop_integration/status_field_config_mini_250220.py
+++ random_tests/25mar_minibbop_integration/status_field_config_mini_250220.py
@@ -5,17 +5,29 @@ sys.path.append(os.path.abspath('..'))
 
 STATUS_FIELDS_CONFIG = {
 
+    # 'minian': {
+    #     'default': 0,
+    #     'conditions': [
+    #         {
+    #             'condition': lambda **kwargs: os.path.exists(
+    #                 os.path.join(kwargs['subfolder_path'], 'minian_dataset.nc')
+    #             ),
+    #             'value': 1  # Mark 1 if "minian_dataset.nc" is found
+    #         }
+    #     ]
+    # },
+
     'minian': {
         'default': 0,
         'conditions': [
             {
-                'condition': lambda **kwargs: os.path.exists(
-                    os.path.join(kwargs['subfolder_path'], 'minian_dataset.nc')
+                'condition': lambda **kwargs: any(
+                    fname.endswith('.nc') for fname in os.listdir(kwargs['subfolder_path'])
                 ),
-                'value': 1  # Mark 1 if "minian_dataset.nc" is found
+                'value': 1  # Mark 1 if any .nc file is found
             }
         ]
-    },
+        },
 
     'valid_rec': {
         # If we want “0” for missing 4.avi and “1” if found:
