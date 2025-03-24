5f84d3b3b124c3c7c832e37c91ad971849efb3d7
gadsfly <amabrodn@gmail.com>
2025-03-24 17:25:29 -0400

    daily update, some modifications on bbop-mini still working on the confition mapping and validatrion after getting mannual recognitions..

--- random_tests/25mar_minibbop_integration/manual_config.py
+++ random_tests/25mar_minibbop_integration/manual_config.py
@@ -1,22 +1,126 @@
-# manual_config.py
 
 sessions = [
     {
-        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241224PMCLE1/customEntValHere/2025_02_13/11_07_37/My_V4_Miniscope"
-        "wnd_size": 1500,
-        "stp_size": 700,
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241224PMCLE1/customEntValHere/2025_02_13/11_07_37/My_V4_Miniscope",
         "max_wnd": 15,
         "diff_thres": 3.0,
-        "pnr_thresh": "auto"
+    },
+
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240916-V1-R1/customEntValHere/2024_10_14/16_54_16/My_V4_Miniscope",
+        "script": "param_search_driver.py"
     },
     {
-        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20250110PMCLE2/customEntValHere/2025_01_15/10_05_30/My_V4_Miniscope",
-        "animal": "20250110PMCLE2",
-        "wnd_size": 1000,
-        "stp_size": 700,
-        "max_wnd": 25,
-        "diff_thres": 3.5,
-        "pnr_thresh": 1.1
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240916-V1-R2/customEntValHere/2024_10_25/14_31_19/My_V4_Miniscope",
+        "script": "param_search_driver.py"
     },
-    # Add as many session configurations as you like...
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241002-pmc-r2/customEntValHere/2024_10_25/16_27_28/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241002-PMC-R1/customEntValHere/2024_10_25/15_02_11/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241002-PMC-R1/customEntValHere/2024_10_24/13_07_09/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241001-PMC-r2/customEntValHere/2024_10_24/16_20_26/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241001-PMC-r2/customEntValHere/2024_10_24/15_27_55/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/202401015-PMC-r2/customEntValHere/2024_11_06/16_57_35/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/202401015-PMC-r2/customEntValHere/2024_11_07/13_04_45/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/202401015-PMC-r2/customEntValHere/2024_11_07/13_25_34/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_17/14_09_19/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_31/11_31_56/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_31/11_50_32/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_11_01/14_40_33/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_11_01/14_57_45/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240910-V1-R/customEntValHere/2024_11_01/13_09_59/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240916-V1-R1/customEntValHere/2024_10_14/14_54_12/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240916-V1-R1/customEntValHere/2024_10_14/16_38_19/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241002-pmc-r2/customEntValHere/2024_10_25/15_50_39/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    },
+    {
+        "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20241002-pmc-r2/customEntValHere/2024_10_25/17_09_14/My_V4_Miniscope",
+        "script": "param_search_driver.py"
+    }
+]
+
+
+
+future_sessions = [
+    #we don't need clean up unless they need to be optimized becuase there is corrsponding recordings i guess. stupid.
+    # {
+    #    "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_08/19_44_49/My_V4_Miniscope",
+    #     "script": "cleanup_session.py",
+    #    "keep_combination": "wnd7000_stp700_max25_diff3.5_pnrauto" #"wnd1500_stp700_max15_diff3.5_pnrauto"
+ 
+    # },
+    # {
+    #     "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_08/20_00_58/My_V4_Miniscope",
+    #     "script": "cleanup_session.py",
+    #    "keep_combination": "wnd7000_stp700_max25_diff3.5_pnr1.1" #"wnd1500_stp700_max15_diff3.5_pnrauto"
+
+
+    # },
+    #     {
+    #     "session_dir": "rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_08/20_32_38/My_V4_Miniscope",
+    #     "script": "cleanup_session.py",
+    #    "keep_combination": "" #"wnd1500_stp700_max15_diff3.5_pnrauto"
+    #     },
+    # {
+    #     "session_dir": "/data/big_rim/rsync_dcc_sum/Oct3V1mini_sorted/20240819-V1-r1/customEntValHere/2024_10_17/14_25_32/My_V4_Miniscope",
+    #     "script": "cleanup_session.py",
+    #     "keep_combination": "" 
+    # },
+    {
+        
+    },
+    # {
+    #     "session_dir": "",
+    #     "script": "cleanup_session.py",
+    #    "keep_combination": "" #"wnd1500_stp700_max15_diff3.5_pnrauto"
+    #     },
 ]
