b5e7e96024305800c7b6846795af7d44a977c874
gadsfly <amabrodn@gmail.com>
2025-05-23 10:47:12 -0400

    commit before merge and future processing

--- utlis/sync_utlis/sync_df_utlis.py
+++ utlis/sync_utlis/sync_df_utlis.py
@@ -276,6 +276,10 @@ def process_sync(base_folder, threshold=3, max_frames=100, min_frame=0):
     if calib_file is None:
         print('No calib file after mir_generate_param is found. Please generate it first.')
         return
+    
+    if os.path.basename(calib_file).startswith('df_'):
+        print(f"Calibration file {calib_file} already has 'df_' prefix. Skipping processing.")
+        return False
 
     # Check for corrupted video files
     for camera in cameras:
@@ -301,7 +305,7 @@ def process_sync(base_folder, threshold=3, max_frames=100, min_frame=0):
     
     calib_nammm = os.path.basename(calib_file)
     folder_name = os.path.basename(base_folder)
-    save_path = os.path.join(base_folder, f'df_synced_{folder_name}_{calib_nammm}')
+    save_path = os.path.join(base_folder, f'df_synced_{calib_nammm}')
 
     # Align frames and process calibration data
     try:
